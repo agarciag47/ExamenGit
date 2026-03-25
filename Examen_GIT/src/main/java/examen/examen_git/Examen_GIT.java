18e493dac4afaea20a20a01112e00f643daf488f
Alejandro Garcia <agarciag47@educantabria.es>
2026-03-25 15:52:48 +0100

    realizado el primer commit

---
+++ Examen_GIT/src/main/java/examen/examen_git/Examen_GIT.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package examen.examen_git;
+
+/**
+ *
+ * @author Alumno
+ */
+public class Examen_GIT {
+
+    public static void main(String[] args) {
+        System.out.println("Alejandro Garcia");
+    }
+}
